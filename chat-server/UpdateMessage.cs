74125fc109caf79056110bee2d809ebf013e30cc
pinx <marcel.van.pinxteren@gmail.com>
2021-01-03 22:12:50 +0100

    Lock/Release event type

--- chat-server/UpdateMessage.cs
+++ chat-server/UpdateMessage.cs
@@ -2,10 +2,17 @@
 
 namespace chat_server
 {
+    public enum EventType
+    {
+        Lock,
+        Release
+    }
+
     public class UpdateMessage
     {
         public string ConnectionId { get; set; }
         public int EventId { get; set; }
+        public EventType EventType { get; set; }
         public DateTime StartAt { get; set; }
         public DateTime DateTime { get; set; }
     }
